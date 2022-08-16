d0af9eef6ef8aabbde78ba05bf716f99c6512470
yooDeve <janhold@naver.com>
2022-08-16 13:24:20 +0900

    all

---
+++ components/NavBar.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from './NavBar.module.css'
+
+export default function NavBar() {
+  const router = useRouter();
+
+  return (
+    <nav>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>about</a>
+      </Link>
+      <style jsx>{`
+      nav {
+          background-color : tomato;
+      }
+      a {
+          text-decoration:none;
+      }
+      `}</style>
+    </nav>
+  );
+}
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar";
+
+export default function about () {
+
+    return (
+    <>
+    <NavBar />
+    <h1>About</h1>
+    </>
+    )
+}
